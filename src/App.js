43743192c25cd070973b4593cd857a1f7c5aa434
HaesungKang <koh1260@naver.com>
2023-01-20 03:30:08 +0900

    useState set array, to do list

--- src/App.js
+++ src/App.js
@@ -1,13 +1,39 @@
 import Button from "./Button";
-import styles from './App.module.css'
+import styles from "./App.module.css";
+import { useState, useEffect } from "react";
 
 function App() {
+  const [toDo, setToDo] = useState("");
+  const [toDos, setToDos] = useState([]);
+  const onChange = (event) => setToDo(event.target.value);
+  const onSubmit = (event) => {
+    event.preventDefault();
+    if (toDo === "") {
+      return;
+    }
+    setToDos((currentArray) => {
+      return [...currentArray, toDo];
+    });
+    setToDo("");
+  };
   return (
     <div>
-      <h1 className={styles.title}>Welcome</h1>
-      <Button text={"Continues"}/>
+      <h1>My To Dos ({toDos.length})</h1>
+      <form onSubmit={onSubmit}>
+        <input
+          onChange={onChange}
+          value={toDo}
+          type="text"
+          placeholder="Write To do"
+        ></input>
+        <button>Add To Do</button>
+      </form>
+      <hr />
+      <h3>할 일 목록</h3>
+      <ul>
+        {toDos.map((todo, index) => <li>{todo}</li>)}
+      </ul>
     </div>
   );
 }
-
 export default App;
